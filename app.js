386920da32a850a6f059272ef0e17e0faf54e163
G. Ferry <grgsferry@gmail.com>
2022-07-05 20:32:23 +0700

    fix authentication

--- app.js
+++ app.js
@@ -1,11 +1,64 @@
 const express = require("express");
-const routes = require("./routes/main-router.js");
-
+const qrcode = require("qrcode-terminal");
+const fs = require("fs");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const app = express();
+
+const clientId = "client-one";
+
+const worker = `./.wwebjs_auth/session-${clientId}/Default/Service Worker`;
+if (fs.existsSync(worker)) {
+  fs.rmSync(worker, { recursive: true });
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", routes);
+const client = new Client({
+  authStrategy: new LocalAuth({ clientId }),
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox"],
+  },
+});
+
+client.initialize();
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("authenticated", () => {
+  console.log("AUTHENTICATED");
+});
+
+client.on("auth_failure", (msg) => {
+  console.error("AUTHENTICATION FAILURE", msg);
+});
+
+process.on("SIGINT", async () => {
+  console.log("(SIGINT) Shutting down...");
+  await client.destroy();
+  process.exit(0);
+});
+
+client.on("ready", () => {
+  console.log("READY");
+});
+
+client.on("disconnected", (reason) => {
+  console.log("Client was logged out", reason);
+});
+
+app.post("/send-message", async (req, res) => {
+  const number = `${req.body.number}@c.us`;
+  const message = req.body.message;
+  try {
+    await client.sendMessage(number, message);
+    res.json({ number, message });
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 app.listen(8000, () => console.log("App is starting."));
--- routes/main-router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/main-controller.js");
-
-router.post("/send-message", controller.sendMessage);
-
-module.exports = router;
